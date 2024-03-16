ed5d6be8111f8467df66e8856933f53007968286
MartinSeoanePosz <martinseo.1996@gmail.com>
2024-03-16 00:18:11 -0300

    render as json

--- Entrega8/src/routes/mockRoutes.js
+++ Entrega8/src/routes/mockRoutes.js
@@ -3,20 +3,29 @@ import {listOfProducts} from '../helpers/mockProducts.js';
 
 const router = express.Router();
 
+// router.get('/', async (req, res) => {
+//     try{
+//     const {products} = listOfProducts();
+//     // console.log(products);
+//     res.render("mockProducts",{
+//         title: "Mocking Products",
+//         data: products,
+//         style: "../css/products.css",
+//     });
+// }catch (err) {
+//     console.error("Error:", err);
+//     res.status(500).send("Internal Server Error");
+// }
+// });
 router.get('/', async (req, res) => {
     try{
     const {products} = listOfProducts();
     // console.log(products);
-    res.render("mockProducts",{
-        title: "Mocking Products",
-        data: products,
-        style: "../css/products.css",
-    });
-}
-catch (err) {
+    res.json(products);
+    }catch (err) {
     console.error("Error:", err);
     res.status(500).send("Internal Server Error");
-}
+    }
 });
 
 export default router;
